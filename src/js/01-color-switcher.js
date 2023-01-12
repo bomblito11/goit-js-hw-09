b00d9ccad8665a0c1a74abd42105ca002862ea25
bomblito11 <maciekswiszcz11@gmail.com>
2023-01-12 22:06:52 +0100

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
-test;
+const startChanging = document.querySelector('[data-start]');
+const stopChanging = document.querySelector('[data-stop]');
+let intervalId;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeColor() {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    startChanging.disabled = true;
+  }, 500);
+}
+
+function stopChangeColor() {
+  clearInterval(intervalId);
+  startChanging.disabled = false;
+}
+
+startChanging.addEventListener('click', changeColor);
+
+stopChanging.addEventListener('click', stopChangeColor);
