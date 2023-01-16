76590d75aa4f7bb010f2d4b67fe38bff95d22bd2
bomblito11 <maciekswiszcz11@gmail.com>
2023-01-16 23:14:00 +0100

    third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+import { delay, reject } from 'lodash';
+import Notiflix from 'notiflix';
+
+//
+const delayInput = document.querySelector('input[name=delay]');
+const stepInput = document.querySelector('input[name=step]');
+const amountInput = document.querySelector('input[name=amount]');
+const formElement = document.querySelector('form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+formElement.addEventListener('submit', e => {
+  e.preventDefault();
+  for (let i = 0; i < amountInput.value; i++) {
+    createPromise(i + 1, Number(delayInput.value) + Number(stepInput.value) * i)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+});
