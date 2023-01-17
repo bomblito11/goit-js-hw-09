f54a262eab5b03b6caeba53aa2e68164005a8c30
bomblito11 <maciekswiszcz11@gmail.com>
2023-01-17 21:36:40 +0100

    color switcher update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,16 +6,20 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+stopChanging.disabled = true;
+
 function changeColor() {
   intervalId = setInterval(() => {
     document.body.style.backgroundColor = getRandomHexColor();
     startChanging.disabled = true;
+    stopChanging.disabled = false;
   }, 500);
 }
 
 function stopChangeColor() {
   clearInterval(intervalId);
   startChanging.disabled = false;
+  stopChanging.disabled = true;
 }
 
 startChanging.addEventListener('click', changeColor);
