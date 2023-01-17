b6b2f164c5cf32a64d5698d61fded83d10bd885b
bomblito11 <75113564+bomblito11@users.noreply.github.com>
2023-01-17 20:37:11 +0000

    Deploying to gh-pages from @ bomblito11/goit-js-hw-09@f54a262eab5b03b6caeba53aa2e68164005a8c30 🚀

---
+++ 01-color-switcher.e6b15d60.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");d.disabled=!0,e.addEventListener("click",(function(){t=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16)),e.disabled=!0,d.disabled=!1}),500)})),d.addEventListener("click",(function(){clearInterval(t),e.disabled=!1,d.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.e6b15d60.js.map
